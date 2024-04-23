9146a223a3962a6b0f2c2d310e4fe7616bd5c8c9
zy <854865755@163.com>
2024-04-23 16:13:58 +0800

    add fishbig recipe

--- src/main/java/com/asdflj/ae2thing/loader/RecipeLoader.java
+++ src/main/java/com/asdflj/ae2thing/loader/RecipeLoader.java
@@ -1,6 +1,7 @@
 package com.asdflj.ae2thing.loader;
 
 import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.BACKPACK_MANAGER;
+import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.BLOCK_FISH_BIG;
 import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.ITEM_INFINITY_CELL;
 import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.ITEM_INFINITY_FLUID_CELL;
 
@@ -19,6 +20,7 @@ public class RecipeLoader implements Runnable {
         1,
         58);
     public static final ItemStack DIAMOND = new ItemStack(Items.diamond, 1);
+    public static final ItemStack FISH = new ItemStack(Items.fish);
     public static final ItemStack AE2_DIGITAL_SINGULARITY_CELL = new ItemStack(
         GameRegistry.findItem("appliedenergistics2", "item.ItemExtremeStorageCell.Singularity"),
         1);
@@ -49,5 +51,6 @@ public class RecipeLoader implements Runnable {
                 "CCC",
                 'C',
                 AE2FC_DIGITAL_SINGULARITY_CELL));
+        GameRegistry.addRecipe(new ShapedOreRecipe(BLOCK_FISH_BIG.stack(), "FFF", "F F", "FFF", 'F', FISH));
     }
 }
