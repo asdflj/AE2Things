1e519d72858b9ae34353769bde49efc2ed46e6ea
zy <854865755@163.com>
2024-04-28 17:35:14 +0800

    fix recipe error

--- src/main/java/com/asdflj/ae2thing/loader/RecipeLoader.java
+++ src/main/java/com/asdflj/ae2thing/loader/RecipeLoader.java
@@ -5,6 +5,7 @@ import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.BLOCK_FISH_BIG;
 import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.ITEM_INFINITY_CELL;
 import static com.asdflj.ae2thing.loader.ItemAndBlockHolder.ITEM_INFINITY_FLUID_CELL;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
@@ -14,11 +15,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class RecipeLoader implements Runnable {
 
     public static final RecipeLoader INSTANCE = new RecipeLoader();
-    public static final ItemStack CHEST = new ItemStack(GameRegistry.findItem("minecraft", "chest"), 1, 54);
-    public static final ItemStack CRAFTING_TABLE = new ItemStack(
-        GameRegistry.findItem("minecraft", "crafting_table"),
-        1,
-        58);
+    public static final ItemStack CHEST = new ItemStack(Blocks.chest, 1);
+    public static final ItemStack CRAFTING_TABLE = new ItemStack(Blocks.crafting_table, 1);
     public static final ItemStack DIAMOND = new ItemStack(Items.diamond, 1);
     public static final ItemStack FISH = new ItemStack(Items.fish);
     public static final ItemStack AE2_DIGITAL_SINGULARITY_CELL = new ItemStack(
