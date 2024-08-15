aa2dcd2ae5ff6df23f1a7eef0264ffb489b6ead9
zy <854865755@163.com>
2024-08-15 19:52:39 +0800

    remove player crafting finish alert

--- src/main/java/com/asdflj/ae2thing/coremod/hooker/CoreModHooks.java
+++ src/main/java/com/asdflj/ae2thing/coremod/hooker/CoreModHooks.java
@@ -1,84 +1,18 @@
 package com.asdflj.ae2thing.coremod.hooker;
 
-import java.lang.reflect.Field;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 
-import com.asdflj.ae2thing.AE2Thing;
 import com.asdflj.ae2thing.common.parts.PartThaumatoriumInterface;
 import com.asdflj.ae2thing.common.tile.TileInfusionInterface;
 import com.asdflj.ae2thing.inventory.EssentiaInventoryAdaptor;
 import com.asdflj.ae2thing.inventory.ThaumatoriumInventoryAdapter;
-import com.asdflj.ae2thing.network.SPacketMEItemInvUpdate;
-import com.asdflj.ae2thing.util.Util;
 
-import appeng.api.networking.IGrid;
-import appeng.api.networking.security.BaseActionSource;
-import appeng.api.networking.security.PlayerSource;
 import appeng.api.parts.IPart;
-import appeng.api.storage.data.IAEItemStack;
-import appeng.me.cluster.implementations.CraftingCPUCluster;
 import appeng.util.InventoryAdaptor;
 import thaumcraft.common.tiles.TileThaumatorium;
 
 public class CoreModHooks {
-
-    public static void craftingComplete(CraftingCPUCluster cpuCluster) {
-        try {
-            if (cpuCluster.isBusy()) return;
-            Field f;
-            f = cpuCluster.getClass()
-                .getField("player");
-            EntityPlayer player = (EntityPlayer) f.get(cpuCluster);
-
-            if (player != null && cpuCluster.getTiles()
-                .hasNext()) {
-                IGrid host = cpuCluster.getTiles()
-                    .next()
-                    .getGridNode(ForgeDirection.UNKNOWN)
-                    .getGrid();
-
-                IGrid wHost = Util.getWirelessGrid(player);
-                if (host != null && host.equals(wHost)) {
-                    f = cpuCluster.getClass()
-                        .getField("requestItem");
-                    IAEItemStack is = (IAEItemStack) f.get(cpuCluster);
-                    if (is != null) {
-                        SPacketMEItemInvUpdate piu = new SPacketMEItemInvUpdate((byte) -2);
-                        piu.appendItem(is);
-                        AE2Thing.proxy.netHandler.sendTo(piu, (EntityPlayerMP) player);
-                    }
-                }
-            }
-        } catch (Exception ignored) {
-
-        }
-    }
-
-    public static void craftingAddActionSource(CraftingCPUCluster cpuCluster, BaseActionSource src) {
-        try {
-            Field f;
-            f = cpuCluster.getClass()
-                .getField("player");
-            if (src instanceof PlayerSource) {
-                f.set(cpuCluster, ((PlayerSource) src).player);
-            } else {
-                f.set(cpuCluster, null);
-            }
-            f = cpuCluster.getClass()
-                .getField("requestItem");
-            IAEItemStack is = cpuCluster.getFinalOutput();
-            if (is != null) {
-                f.set(cpuCluster, is.copy());
-            }
-
-        } catch (Exception ignored) {
-
-        }
-    }
 
     public static InventoryAdaptor getAdaptor(TileEntity tile, ForgeDirection face) {
         if (tile == null) return null;
