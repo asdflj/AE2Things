fc2c418201eefb8cf25d9c93c9f263a60a2f68b0
zy <854865755@163.com>
2025-02-27 17:31:47 +0800

    update

--- src/main/java/com/asdflj/ae2thing/coremod/mixin/nei/MixinGuiContainerManager.java
+++ src/main/java/com/asdflj/ae2thing/coremod/mixin/nei/MixinGuiContainerManager.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fluids.FluidStack;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -44,7 +43,6 @@ public abstract class MixinGuiContainerManager {
     @Shadow(remap = false)
     public GuiContainer window;
 
-    @Unique
     private static RenderItem ae2thing$r = new RenderItem();
 
     private static ItemStack ae2Thing$lastStack = null;
@@ -88,7 +86,6 @@ public abstract class MixinGuiContainerManager {
 
     }
 
-    @Unique
     private void ae2thing$render(IAEItemStack item, int x, int y) {
         ItemStack stack = item.getItemStack();
         GL11.glPushMatrix();
