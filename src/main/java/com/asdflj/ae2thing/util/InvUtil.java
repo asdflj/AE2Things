d55904b3d213901b7178d5f7746d0c54c26c7d89
zy <854865755@163.com>
2025-12-27 14:14:35 +0800

    spotlessApply

--- src/main/java/com/asdflj/ae2thing/util/InvUtil.java
+++ src/main/java/com/asdflj/ae2thing/util/InvUtil.java
@@ -7,6 +7,7 @@ import java.util.function.Predicate;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public class InvUtil {
@@ -37,19 +38,20 @@ public class InvUtil {
         return false;
     }
 
-    public ImmutablePair<Integer,ItemStack> find(Predicate<ItemStack> item){
+    public ImmutablePair<Integer, ItemStack> find(Predicate<ItemStack> item) {
         for (IInv obj : INVENTORY) {
             IInventory inv = obj.getInventory(player);
             for (int i = 0; i < inv.getSizeInventory(); i++) {
                 ItemStack stack = inv.getStackInSlot(i);
                 if (item.test(stack)) {
-                    return new ImmutablePair<>(i,stack);
+                    return new ImmutablePair<>(i, stack);
                 }
             }
         }
         return null;
     }
-    public static ImmutablePair<Integer,ItemStack> find(EntityPlayer player, Predicate<ItemStack> item){
+
+    public static ImmutablePair<Integer, ItemStack> find(EntityPlayer player, Predicate<ItemStack> item) {
         return new InvUtil(player).find(item);
     }
 
